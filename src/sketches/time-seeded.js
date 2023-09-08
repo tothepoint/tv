1a3ab40dac3aa46dea95fdd80d0fac63238fd8f8
Gedas Lukšas <luksas.gedas@gmail.com>
2023-09-08 20:13:46 +0300

    Try not to compensate for the time sync request roundript time, make a "priming" request before making one which is used.

--- src/sketches/time-seeded.js
+++ src/sketches/time-seeded.js
@@ -31,6 +31,10 @@ let timeSeededSketch = function (p) {
         let response;
         if (useServerTime) {
             // response = await fetch('http://worldtimeapi.org/api/timezone/UTC');
+
+            // Do a call to "prime" the communication.
+            response = await fetch('https://timeapi.io/api/Time/current/zone?timeZone=UTC', { mode: 'no-cors' });
+
             response = await fetch('https://timeapi.io/api/Time/current/zone?timeZone=UTC', { mode: 'no-cors' });
         } else {
             // Fallback to the client's time instead of server's in case of an error.
@@ -69,7 +73,8 @@ let timeSeededSketch = function (p) {
                 const requestDurationMs = requestEndTime - requestStartTime;
                 // Calculate client time offset to server time.
                 // Always add that offset to compensate.
-                const nowServer = new Date(new Date(data.datetime || data.dateTime).getTime() + requestDurationMs / 2);
+                // const nowServer = new Date(new Date(data.datetime || data.dateTime).getTime() + requestDurationMs / 2);
+                const nowServer = new Date(new Date(data.datetime || data.dateTime).getTime());
                 const nowClient = new Date();
 
                 offsetFromServerTimeMs = nowClient.getTime() - nowServer.getTime();
