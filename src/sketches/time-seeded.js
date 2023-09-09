6b82ca74008b848f4df19fb64c8fbb693f33214b
Gedas Lukšas <luksas.gedas@gmail.com>
2023-09-09 13:30:38 +0300

    Try snapshoting client time before the time request.

--- src/sketches/time-seeded.js
+++ src/sketches/time-seeded.js
@@ -81,6 +81,8 @@ let timeSeededSketch = function (p) {
         recalcMapSize();
 
         const requestStartTime = new Date();
+        const nowClient = new Date();
+
         loadUTCTimeBasedSeed()
             .then((data) => {
                 const requestEndTime = new Date();
@@ -89,7 +91,6 @@ let timeSeededSketch = function (p) {
                 // Always add that offset to compensate.
                 // const nowServer = new Date(new Date(data.datetime || data.dateTime).getTime() + requestDurationMs / 2);
                 const nowServer = new Date(new Date(data.datetime || data.dateTime).getTime());
-                const nowClient = new Date();
 
                 const nowClientTimestamp = nowClient.getTime();
                 const nowServerTimestamp = nowServer.getTime();
