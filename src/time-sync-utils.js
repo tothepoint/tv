d5e0a4239652f6acbf3293a2657dd151d66c4b39
Gedas Lukšas <luksas.gedas@gmail.com>
2025-04-10 22:34:15 +0300

    Updated the time server to an alive one and updated the fallback to current client time.

--- src/time-sync-utils.js
+++ src/time-sync-utils.js
@@ -1,4 +1,5 @@
 let offsetFromServerTimeMsCached;
+let useServerTime = true;
 
 const getClientTimeOffsetFromServer = (clientTimestamp, serverTimestamp) => {
     const offsetFromServerTimeMs = serverTimestamp - clientTimestamp;
@@ -7,7 +8,6 @@ const getClientTimeOffsetFromServer = (clientTimestamp, serverTimestamp) => {
 };
 
 const loadTimeUTC = async () => {
-    let useServerTime = true;
     let response;
     if (useServerTime) {
         // Do a call to "prime" the communication.
@@ -34,6 +34,32 @@ const loadTimeUTC = async () => {
     return data;
 };
 
+const loadTimeUTCPostmanAPI = async () => {
+    let response;
+    if (useServerTime) {
+        response = await fetch('https://postman-echo.com/time/now');
+    } else {
+        // Fallback to the client's time instead of server's in case of an error.
+        return { datetime: new Date().getTime() };
+    }
+
+    if (!response.ok) {
+        timeSyncError = true;
+        console.log('Time sync error, server response:', response);
+        // const message = `Error: ${response.status}`;
+        // throw new Error(message);
+
+        // Fallback to the client's time instead of server's in case of an error.
+        return { datetime: new Date().getTime() };
+    }
+
+    const data = await response.json();
+    const datetime = new Date(data);
+    const serverTimestamp = datetime.getTime();
+
+    return { datetime: serverTimestamp };
+}
+
 const loadAndCalculateTimeOffsetFromServerMs = (forceClearCache) => {
     const promise = new Promise((resolve, reject) => {
         // Cache the calculated time offset to reduce server calls.
@@ -42,26 +68,32 @@ const loadAndCalculateTimeOffsetFromServerMs = (forceClearCache) => {
             return;
         }
 
-        const requestStartTime = new Date();
-        loadTimeUTC()
+        const onTimeLoaded = (data) => {
+            const requestEndTime = new Date();
+            // Calculate client time offset to server time.
+            // Always add that offset to compensate.
+            const nowServer = new Date(new Date(data.datetime).getTime());
+            const nowClient = new Date();
+
+            const nowClientTimestamp = nowClient.getTime();
+            const nowServerTimestamp = nowServer.getTime();
+            offsetFromServerTimeMsCached = getClientTimeOffsetFromServer(nowClientTimestamp, nowServerTimestamp);
+
+            resolve(offsetFromServerTimeMsCached);
+        };
+
+        loadTimeUTCPostmanAPI()
             .then((data) => {
-                const requestEndTime = new Date();
-                const requestDurationMs = requestEndTime - requestStartTime;
-                // Calculate client time offset to server time.
-                // Always add that offset to compensate.
-                const nowServer = new Date(new Date(data.datetime).getTime());
-                const nowClient = new Date();
-
-                const nowClientTimestamp = nowClient.getTime();
-                const nowServerTimestamp = nowServer.getTime();
-                offsetFromServerTimeMsCached = getClientTimeOffsetFromServer(nowClientTimestamp, nowServerTimestamp);
-
-                resolve(offsetFromServerTimeMsCached);
+                onTimeLoaded(data);
             })
             .catch(err => {
-                console.log(`Got an error fetching time: ${err}`);
-            });
+                console.log(`Falling back to using client time. Got an error fetching server time: ${err}`);
 
+                // Fallback to the client's time instead of server's in case of an error.
+                const nowClient = new Date();
+                const nowClientTimestamp = nowClient.getTime();
+                onTimeLoaded({ datetime: nowClientTimestamp });
+            });
     });
 
     return promise;
